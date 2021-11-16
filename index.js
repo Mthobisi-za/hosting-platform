6013830a50beeefeb49801ba21f8cab9c6a8f855
Mthobisi-za <mthobisimtho333@gmail.com>
2021-11-16 11:06:42 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const {Pool} = require("pg");
+const { engine } = require('express-handlebars');
+const body = require("body-parser");
+
+
+///config modules
+app.use(express.static("public"));
+app.use(body.json());
+app.use(body.urlencoded({extended: false}));
+app.set("view engine", "handlebars");
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set("views", "./views");
+
+
+app.get("/", (req,res)=>{
+    res.render('index');
+})
+
+app.listen(PORT, ()=>{
+    console.log("App started on " + PORT);
+});
